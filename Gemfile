6bfab9c7d0ddedd07411c3089faa05cdc2658438
EC2 Default User <ec2-user@ip-172-31-10-98.ap-southeast-1.compute.internal>
2020-02-24 07:35:38 +0000

    投稿機能の完成

--- Gemfile
+++ Gemfile
@@ -7,3 +7,8 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 gem "sinatra"
 gem "sinatra-reloader"
+gem "sinatra-activerecord"
+gem 'activerecord', '~> 5.2.0'
+gem "sqlite3"
+gem "rake"
+gem "pry"
--- pictweet.rb
+++ pictweet.rb
@@ -3,9 +3,27 @@ require 'bundler'
 
 Bundler.require
 
+set :database, {adapter: "sqlite3", database: "tweets.sqlite3"}
+
+class Tweet < ActiveRecord::Base
+end
+
 get "/" do
 
-    @random = rand(6) + 1
+    @tweets = Tweet.all
     erb :index
+
+end
+
+get "/tweets/new" do
+
+   erb :new 
+
+end
+
+post "/tweets" do
+    
+    Tweet.create(params)
+    redirect '/'
 
 end
